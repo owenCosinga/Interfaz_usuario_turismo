c15cf803089ea9fdc5ebcbaaf49dd051c989743c
owenCosinga <owenpsicosis@gmail.com>
2022-03-16 16:45:30 -0500

    este es el avanze de la interfaz de los lugares turisticos de la region de cuzco espero les guste

--- lib/Models/Destination.dart
+++ lib/Models/Destination.dart
@@ -13,27 +13,27 @@ class Destination {
 final List<Destination> destinations = [
   Destination(
       image: 'assets/pyramids.jpg',
-      name: 'Egypt',
+      name: 'Salkantay',
       description:
-          'You\'ll have seen a thousand photographs of the Pyramids of Giza by the time you finally get here, but nothing beats getting up-close-and-personal with these ancient monuments. Egypt\'s most famed and feted structures, these ancient tombs of kings guarded by the serene Sphinx have wowed spectators for centuries.'),
+          'Salkantay Trek es una caminata en América del Sur y una alternativa al Camino Inca tradicional para llegar a Machupicchu . Presenta una combinación única de cultura y mística, conservada con orgullo por el pueblo peruano, y es una aventura asombrosa y de extraordinaria belleza natural. Salkantay trekking se lleva a cabo en los picos más altos de la montaña Humantay , donde el frío hielo de las montañas se encuentra con el calor humeante de los Andes . La caminata está rodeada por el histórico Machupicchu (Parque Nacional y es considerada una de las 25 mejores caminatas del mundo). Salkantay trek es una caminata muy diversa y rica en geografía, cultura e historia andina. losLa Cordillera de Vilcanota contiene las dos veinte montañas más altas de América.'),
   Destination(
       image: 'assets/maldives.jpg',
-      name: 'Maldives',
+      name: 'Saqsaywaman',
       description:
-          'Located just south of the Indian subcontinent, the Maldives are a gorgeous chain of islands in the Indian Ocean-Arabian Sea area consisting of 26 atolls. Travel to the Maldives and see why the islands have become so popular in recent decades (especially as a honeymoon destination) and why Maldives travel is always an unforgettable experience.'),
+          'Saqsaywaman esta ubicado al interior de los cuadrantes del Chinchaysuyu y el Antisuyu.es una de las edificaciones más asombrosas de todo el mundo. Se piensa que empezó a construirse durante el gobierno del inca Pachacutec en el siglo XV.Se cree que más de 20 mil hombres extrajeron las piedras de las canteras aledañas y la trasladaron 20 kilómetros hasta la colina de la ciudad del Cusco. Hoy, se calcula que Sacsayhuaman conserva solo el 40 por ciento de su antigua estructura. Aun así, el sitio posee estructuras de hasta 125 toneladas de peso. ¿Cómo los incas construyeron Sacsayhuaman con estructuras de un peso y tamaño inverosímiles para la época? Muchas preguntas aún son un misterio. Ahí reside la belleza de este sitio arqueológico.'),
   Destination(
       image: 'assets/paris.jpg',
-      name: 'Paris',
+      name: 'Qorikancha',
       description:
-          'The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.'),
+          'Se ubica la plazoleta de Santo Domingo en el actual recinto del templo y convento de Santo Domingo, construidos sobre los restos del Qorikancha y se extiende hasta la Av. El Sol.Según la concepción inca, el Qorikancha fue el centro religioso del Cusco, centro geográfico y político. El templo del Qorikancha, era el lugar donde rendía pleitesía al máximo dios inca el "Inti" (sol). "Qori" significa oro trabajado, su forma castellanizada es cori. "Kancha" significa lugar cercado, limitado por muros. De donde se tiene que el nombre corresponde aproximadamente "lugar cercado que contiene oro".'),
   Destination(
       image: 'assets/venice.jpg',
-      name: 'Venice',
+      name: 'Plaza de armas de Cusco',
       description:
-          'Venice and its mainland are particularly rich of museums and historical buildings of great artistic and cultural importance. Here you can choose among a wide variety of museums, churches, palaces and villas, Venice historic centre, the famous islands of Murano and Burano, the villas along the Brenta river and the beautiful landscapes the Miranese area, and of course less famous islands in Venice Lagoon.'),
+          'La Plaza de Armas de Cusco te enamora en cuanto la pisas. Sus edificios, de arquitectura colonial, son el reflejo del paso de los conquistadores españoles por esta ciudad. Sin embargo, esta plaza tiene su origen mucho antes de la Conquista. Este fue el punto elegido para comenzar a crear la Ciudad Imperial, la cual se convirtió en capital del Imperio Inca.Hoy en día, la Plaza de Armas es uno de los principales atractivos turísticos de la ciudad de Cusco. Aquí se encuentran diversos templos religiosos (la catedral, la Iglesia de la Compañía de Jesús y la Capilla del Triunfo), acompañados por hermosos portales con arcos coloniales.'),
   Destination(
       image: 'assets/rio.jpg',
-      name: 'Brazil',
+      name: 'Piedra de los 12 angulos',
       description:
-          'Golden beaches and lush mountains, samba-fueled nightlife and spectacular football matches: welcome to the Cidade Maravilhosa (Marvelous City).'),
+          'La piedra de los 12 ángulos está ubicado en la calle Hatun Rumiyoq (piedra grande en lengua quechua), casi 500 metros desde la Plaza de Armas de la ciudad. Forma parte de un muro inca donde actualmente se asienta el palacio Arzobispal y Museo de Arte Religioso del Cusco.La piedra de los 12 ángulos es una de las atracciones más populares de la ciudad del Cusco. Su fama se debe a la perfección de su construcción. Se trata de una enorme piedra pulida, la cual encaja a la perfección en cada una de sus esquinas con el resto de piedras que forman el muro. Se encuentra en la calle Hatun Rumiyoc. La visita a este lugar es gratuita. Es uno de los lugares más fotografiados del Cusco.'),
 ];
--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -17,92 +17,98 @@ class _HomeScreenState extends State<HomeScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Explore',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
-                    ),
-                  ),
-                  Icon(
-                    Icons.notifications,
-                    size: 30,
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(40)),
-                    ),
-                    hintText: 'Where to go next?',
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    )),
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height - 250,
-              child: PageView.builder(
-                  itemCount: destinations.length,
-                  controller: PageController(viewportFraction: 0.7),
-                  onPageChanged: (int index) => setState(() => _index = index),
-                  itemBuilder: (context, i) {
-                    return Transform.scale(
-                      scale: i == _index ? 1.0 : 0.9,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => DestinationScreen(
-                                    destination: destinations[i],
-                                  )));
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                              height: MediaQuery.of(context).size.height - 350,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  image: DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: AssetImage(destinations[i].image),
-                                  ),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.3),
-                                      spreadRadius: 2,
-                                      blurRadius: 2,
-                                      offset: Offset(2, 3),
-                                    )
-                                  ]),
-                            ),
-                            Text(
-                              destinations[i].name,
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Explora',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
                       ),
-                    );
-                  }),
-            )
-          ],
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                      ),
+                      hintText: 'A Donde Vas?',
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      )),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height - 250,
+                child: PageView.builder(
+                    itemCount: destinations.length,
+                    controller: PageController(viewportFraction: 0.7),
+                    onPageChanged: (int index) =>
+                        setState(() => _index = index),
+                    itemBuilder: (context, i) {
+                      return Transform.scale(
+                        scale: i == _index ? 1.0 : 0.9,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => DestinationScreen(
+                                      destination: destinations[i],
+                                    )));
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height - 350,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: AssetImage(destinations[i].image),
+                                    ),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        spreadRadius: 2,
+                                        blurRadius: 2,
+                                        offset: Offset(2, 3),
+                                      )
+                                    ]),
+                              ),
+                              Text(
+                                destinations[i].name,
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
         ),
         bottomNavigationBar: Container(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -120,19 +126,19 @@ class _HomeScreenState extends State<HomeScreen> {
             tabs: [
               GButton(
                 icon: Icons.home,
-                text: 'home',
+                text: 'hogar',
               ),
               GButton(
                 icon: Icons.favorite_border,
-                text: 'favorite',
+                text: 'favorito',
               ),
               GButton(
                 icon: Icons.map,
-                text: 'map',
+                text: 'mapa',
               ),
               GButton(
                 icon: Icons.person_outline,
-                text: 'profile',
+                text: 'perfil',
               ),
             ],
             selectedIndex: _selectedIndex,
